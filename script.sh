87042fd4d9f5c4d3008857f9e61fefc8ff3d893b
Shiv Pratap Singh <106021529+Kirosci@users.noreply.github.com>
2023-08-20 22:30:23 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,35 +1,94 @@
 #!/bin/bash
 
-echo -n "Paste the Domains here: "
-read domains
-mkdir $domains
-clear
+read -p "Enter domain name: " domain
+dir=$domain
+if [ -d $domain ]; then
+read -p "Enter directory name to save results in: " dir
+mkdir $dir
+echo "All results will be saved in" "$dir/" "directory."
+else
+mkdir $dir
+echo "All results will be saved in" "$dir/" "directory."
+fi
 
-# Run commands simultaneously
 (
-  echo "$domains" | assetfinder | tee $domains/asset.txt
+  echo "$domain" | assetfinder | tee $dir/assetfinder_subdomains.txt
 ) &
 
 (
-  echo "$domains" | haktrails subdomains | tee $domains/haktrails.txt
+  echo "$domain" | haktrails subdomains | tee $dir/haktrails_subdomains.txt
 ) &
 
 (
-  echo "$domains" | subfinder -pc /home/kali/.config/subfinder/provider-config.yaml | tee $domains/subfinder.txt
+  echo "$domain" | subfinder | tee $dir/subfinder_subdomains.txt
 ) &
 
 (
-  amass enum -d "$domains" | tee $domains/amass.txt
+  amass enum -d "$domain" | tee $dir/amass_subdomains.txt
 ) &
 
 
-  python3 ../tools/knock/knockpy.py $domains | tee $domains/knockpy_full.txt
-  cat $domains/knockpy_full.txt | awk '{print $5}' | tee $domains/knockpy_subsonly.txt &
+python3 tools/knock/knockpy.py $domain | tee $dir/knockpy_full.txt
+cat $dir/knockpy_full.txt | awk '{print $5}' | tee $dir/knockpy_subdomains.txt &
 
-# Wait for all background processes to finish
 wait
 
-echo "---------------Sorting...--------------------"
-cat $domains/asset.txt $domains/haktrails.txt $domains/subfinder.txt $domains/amass.txt $domains/knockpy_subsonly.txt | sort -u | tee $domains/subdomains.txt
+cat $dir/assetfinder_subdomains.txt $dir/haktrails_subdomains.txt $dir/subfinder_subdomains.txt $dir/amass_subdomains.txt $dir/knockpy_subdomains.txt | sort -u | tee $dir/all_assets.txt &
+
+
+#----------------------------Assets_Sorting_Done-----------------------------------------
+
+
+wait
+
+cat $dir/all_assets.txt | grep $domain | awk '{print$1}' | sort -u | tee $dir/subdomains.txt &
+
+
+#---------------------------Subdomain_Main_Done--------------------------------------------
+
+
+wait
+
+cat $dir/subdomains.txt | httpx | tee $dir/live_subdomains.txt &
+
+
+#-----------------------------Live_Domains_Done---------------------------------------------
+
+
+wait
+
+(
+cat $dir/live_subdomains.txt | waybackurls | tee $dir/wayback_urls.txt
+) &
+
+(
+cat $dir/live_subdomains.txt | gau | tee $dir/gau_urls.txt 
+) &
+
+wait
+
+cat $dir/wayback_urls.txt $dir/gau_urls.txt | sort -u |tee $dir/urls.txt &
+
+
+#-------------------------------------URLs_Done------------------------------------------------
+
+
+wait
+
+mkdir $dir/deep
+
+mv $dir/assetfinder_subdomains.txt $dir/haktrails_subdomains.txt $dir/subfinder_subdomains.txt $dir/amass_subdomains.txt $dir/knockpy_full.txt $dir/knockpy_subdomains.txt $dir/gau_urls.txt $dir/wayback_urls.txt $dir/all_assets.txt $dir/deep/
+
+
+#-----------------------------------------Organizing_Done---------------------------------------
+
+
+clear
+
+echo "Mission Completed Respect+"
+
+tree $dir
+
 
 # Use "chmod 777 script.sh" to give it permissions for soomth run
+# Need to add subdomains takeover check
